012fa6ebae58c7dfa4cd7a0eb657c474cdd119c0
Matthew Peters <matthewpeters.us@icloud.com>
2025-10-17 21:27:29 +0000

    Canvas added (Step 1 complete)

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
+//import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
-//test
-document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
-`;
+
+//document.body.innerHTML = `
+//  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+//`;
+
+// Create and add a title (h1) to the webpage
+const title = document.createElement("h1");
+title.textContent = "Simple Drawing App";
+document.body.appendChild(title);
+
+// Create and add a 256x256 canvas
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+document.body.appendChild(canvas);
